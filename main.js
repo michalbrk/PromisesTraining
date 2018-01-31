37c44836f82a35e0be781986adc488bb2002ea39
michalbrk <brakowski.m@gmail.com>
2018-01-31 22:05:42 +0100

    promises testing using the api requests

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
-//
-//    function sync(callback) {
-//        setTimeout(function() {
-//            
-//            //Deferred function assignement to the constant
-//            const res = deferred();
-//            
-//            //Invoking callback with the deferred function
-//            //as the argument
-//            callback(res);
-//        }, 1000);
-//    }
-//    
-//    function deferred() {
-//        return 'deferred';
-//    }
-//    
-//    //Callback declaration
-//    //and invoking of the sync function
-//    sync(function(result) {
-//        console.log(result);
-//    });
+
+
+    fetch('https://api.spotify.com/v1/search?q=michael+jackson&type=album')
+        .then(response => response.json())
+        .then(data => {
+            const uris = data.albums.items.map(item => {
+                return item.uri;
+            }).slice(0,5);
+
+        const tasks = uris.map(uri => fetch('https://api.spotify.com/v1/albums/' + uri.split(':')[2]));
+        return Promise.all(tasks);
+    }).then(responses => {
+        return Promise.all(responses.map(response => response.json()));
+    }).then(albums => {
+        return Promise.all(albums.map(album => fetch(album.images[0].url)));
+    }).then(responses => {
+       return Promise.all(responses.map(response => response.blob())); 
+    }).then(blobs => {
+
+    });
+
 });
